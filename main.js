2e91b974fd4caeec04fc3b30419c38f180c07641
Mark Hinojosa <markhinojosa1212@gmail.com>
2018-06-22 20:52:51 -0500

    edited project content boxs

--- main.js
+++ main.js
@@ -7,35 +7,49 @@ $(document).ready(function() {
   const portfolioAnchor = $("a[name = portfolioAnchor]").position().top;
 
   $(document).on("scroll", () => {
-    let inView = "";
     const aboutAnchor = $("a[name = aboutAnchor]").position().top;
     const portfolioAnchor = $("a[name = portfolioAnchor]").position().top;
     const scrollHeight = $(document).scrollTop();
-    if (
-      scrollHeight >= aboutAnchor - 100 &&
-      scrollHeight <= portfolioAnchor - 100
-    ) {
-      inView = "about";
-    } else if (scrollHeight >= portfolioAnchor - 100) {
-      console.log("portfolio");
-      inView = "portfolio";
-    } else {
-      $("#portfolioButton").css("color", "white");
-    }
-    if (inView === "about") {
-      $("#portfolioButton").css("color", "white");
-      $("#aboutButton").css("color", "gold");
-    }
-    if (inView === "portfolio") {
-      $("#aboutButton").css("color", "white");
-      $("#portfolioButton").css("color", "gold");
-    }
+
+    const checkCurrentview = () => {
+      if (
+        scrollHeight >= aboutAnchor - 100 &&
+        scrollHeight <= portfolioAnchor - 100
+      ) {
+        changeNavSection("about");
+      } else if (scrollHeight >= portfolioAnchor - 100) {
+        changeNavSection("portfolio");
+      }
+    };
+
+    const changeNavSection = n => {
+      if (n === "about") {
+        return (
+          $("#aboutButton").css("color", "gold"),
+          $("#portfolioButton").css("color", "white")
+        );
+      }
+      if (n === "portfolio") {
+        return (
+          $("#aboutButton").css("color", "white"),
+          $("#portfolioButton").css("color", "gold")
+        );
+      }
+    };
+    checkCurrentview();
   });
 
-  $("#test").mouseover(function() {
-    console.log("line 36");
-    $("#test").css("background-image", "none");
-    // $(".projects__container__item__h__text").attr("data-aos", "fade-in");
-    $(".projects__container__item__h__text").css("z-index", "100");
-  });
+  // $("#test").hover(
+  //   function() {
+  //     console.log("on");
+  //     $(".projects__container__item__h__content").css("opacity", "1");
+  //     $(".projects__container__item__h__content").attr("data-aos", "fade-in");
+
+  //     // $("#test").css("background-image", "none");
+  //   },
+  //   function() {
+  //     console.log("off");
+  //     $(".projects__container__item__h__content").css("opacity", "0");
+  //   }
+  // );
 });
